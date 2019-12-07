ae258f570b9d22a3eaa241786186b27773933d00
Camilo Salazar <camilo@ug.uchile.cl>
2019-12-07 19:09:49 -0300

    Initial navbar setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { LoginModule } from './components/login/login.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +22,13 @@ import { NavbarComponent } from './navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatMenuModule,
+    MatToolbarModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
